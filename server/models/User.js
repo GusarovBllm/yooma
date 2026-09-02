34a51e1f2319f4eb620f9e023edccda72a6453f4
GusarovBllm <skddy16@gmail.com>
2026-09-02 23:56:49 +0300

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  login: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
+  avatar: { type: String, default: '' },
+  banner: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  status: { type: String, default: 'offline' },
+  contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
