c7bcca8f82ca13dce5fd4872fc05724b1f7f4d82
GusarovBllm <skddy16@gmail.com>
2026-09-02 23:56:51 +0300

    Add Message model

---
+++ server/models/Message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, default: '' },
+  file: { type: String, default: '' },
+  fileName: { type: String, default: '' },
+  read: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
